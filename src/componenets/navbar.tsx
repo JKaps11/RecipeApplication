184e808f9e18b5eb38758fc622df50ed5ec7fbad
Josh <kapsjosh11@gmail.com>
2023-12-18 22:09:08 -0500

    starting landing page

---
+++ src/componenets/navbar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styling/navabar.css";
+const Navbar = () => {
+    return <div id="navbarLayout">
+        <button id="navbarButton">
+            Home
+        </button>
+        <button id="navbarButton">
+            Features
+        </button>
+        <button id="navbarButton">
+            About
+        </button>
+    </div>
+}
+
+export default Navbar;
--- src/views/welcome.tsx
+++ src/views/welcome.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import '../styling/welcome.css';
 import LoginButton from "../componenets/loginbutton";
-import {useAuth0} from "@auth0/auth0-react";
-import {useNavigate} from "react-router-dom";
-
-
+import Navbar from "../componenets/navbar";
 const WelcomePage = () => {
-    const  {isAuthenticated } = useAuth0();
-    const navigate = useNavigate();
+    return <div id="welcomelayout">
+        <div id="header">
+            <h1 id="welcomeTitle">Remedy Recipes</h1>
+        </div>
+        <div id="navbar">
+            <Navbar/>
+        </div>
+        <div id="lpcontent">
+            <h1 id="welcometitle">The worlds simplest menu and recipe editor</h1>
+            <h1 id="welcometitletwo">Remedy Recipes</h1>
+            <LoginButton/>
+        </div>
 
-    return isAuthenticated ? <div id="welcomelayout">
-        <h1 id="welcometitle">Welcome to Remedy Recipes</h1>
-        <button id ="welcomebutton" onClick={() => navigate("/home")}>
-            <span id ="welcomespan">
-                Continue
-            </span>
-        </button>
-    </div> : <div id="welcomelayout">
-        <h1 id="welcometitle">Welcome to Remedy Recipes</h1>
-        <LoginButton/>
     </div>
+
 }
 export default WelcomePage;
