80c011ca2eaee3eaa5c182dd3c3390ee0e814a57
Josh <kapsjosh11@gmail.com>
2024-02-16 07:12:28 -0500

    adding css to new grocery list component

--- src/views/grocerylists.tsx
+++ src/views/grocerylists.tsx
@@ -1,18 +1,35 @@
 import React, {useState} from "react";
 import {GroceryList} from "../customTypes";
+import "../styling/grocerylists.css";
 
-const GroceryLists = () =< {
+interface GroceryListEntryProps {
+  gList: GroceryList
+}
+const GroceryListEntry = ({gList}:GroceryListEntryProps) => {
+  return <button id="gListEntryButton">
+      <p>{gList?.Name}</p>
+    </button>
+}
+
+const GroceryLists = () => {
   
   const [groceryLists, setGorceryLists] = useState<Array<GroceryList>>();
   const displayGroceryLists = () => {
-        
+    if(groceryLists?.length === 0){
+      return <h2>Start by creating your first grocery list</h2>
+    }
+    else{
+      return groceryLists?.map((glist) => {
+        return <GroceryListEntry gList={glist}/>
+      })
+    }
   }
   return <div id="groceryListLayout">
     <div id="groceryListTop">
       <div id="groceryListButtonDiv">
         <button id="groceryListAddButton"></button>
       </div>
-      <div>
+      <div id="groceryListTopRight">
       </div>
     </div>
     <div id="groceryListBody">
