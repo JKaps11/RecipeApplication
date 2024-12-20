fe41aab6a4cb74db715dbe9eb8a2e009b17403c8
Jkaps11 <kapsjosh11@gmail.com>
2024-12-19 20:07:52 -0500

    Creating profile update form. Still need to work on design, form functionality, and form backend

--- src/componenets/profile.tsx
+++ src/componenets/profile.tsx
@@ -1,12 +1,41 @@
-import React from 'react';
-import logoutbutton from "./logoutbutton";
+import React, {useRef} from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import {SubmitHandler, useForm} from "react-hook-form"
 import '../styling/profile.css';
-import LogoutButton from "./logoutbutton";
+import CustomDialog from "./customdialog";
+
 const Profile = () => {
     const { user, isAuthenticated , logout} = useAuth0();
+
+    type UserForm = {
+        NickName: string,
+        UserId: string,
+        NewPassword: string,
+        Email: string,
+    }
+
+    const {register, handleSubmit, getValues} = useForm<UserForm>({
+        defaultValues: {
+            NickName: user?.nickname,
+            UserId: user?.sub,
+            NewPassword: "",
+            Email: user?.email
+        }
+    })
+
+    const dialogRef = useRef<HTMLDialogElement>(null)
+    const submitUpdateUserForm = () => {
+        dialogRef.current?.showModal();
+    }
+
+    const handleContinue = () => {
+
+    }
+
     return (
         isAuthenticated ?(
+            <>
+                <CustomDialog ref = {dialogRef} onContinue={() => handleContinue()} title={"Are you sure you want\n to update your user information?"}/>
             <div id={"mainProfileLayout"}>
                 <div id={"mainProfileHeaderLayout"}>
                     <h2 id={"profileHeader"}>Remedy Recipes Account</h2>
@@ -21,16 +50,35 @@ const Profile = () => {
                         <h4 id={"profileEmail"}>{user?.name}</h4>
                     </div>
                     <div id={"rightProfileLayout"}>
-                        <ul>
-                        {user && Object.keys(user).map((objKey, i) => (
-                            <li key={i}>
-                                {objKey}: {user[objKey]}
-                            </li>
-                        ))}
-                        </ul>
+                        {/*Change nickname and change email and change password*/}
+                        <form id={"profileForm"} onSubmit={handleSubmit(submitUpdateUserForm)}>
+                            <div id={"rightProfileBlock"}>
+                                <label id={"profileLabel"}>Nick Name: </label>
+                                <input
+                                    id={"profileInput"}
+                                    {...register('NickName')}
+                                />
+                            </div>
+                            <div id={"rightProfileBlock"}>
+                                <label id={"profileLabel"}>Email: </label>
+                                <input
+                                    id={"profileInput"}
+                                    {...register('Email')}
+                                />
+                            </div>
+                            <div id={"rightProfileBlock"}>
+                                <label id={"profileLabel"}>Password: </label>
+                                <input
+                                    id={"profileInput"}
+                                    {...register('NewPassword')}
+                                />
+                            </div>
+                        </form>
                     </div>
                 </div>
-            </div>) : null
+            </div>
+            </>
+                ) : null
 
     );
 };
